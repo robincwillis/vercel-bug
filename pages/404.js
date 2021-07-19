d7316413092e997006da1e26516a9fc67af5cd07
Robin Willis <robincwillis@gmail.com>
2021-07-19 11:30:53 -0400

    first commit

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Custom404() {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>404</h1>
+    </div>
+  );
+}
